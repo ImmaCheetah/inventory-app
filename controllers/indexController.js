6ab9dae64ea656ab5df86de3c9009f182e6c2d8f
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-10-21 16:05:31 -0400

    feat: add title to index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,11 @@ const db = require("../db/queries");
 async function getIndex(req, res) {
     const cars = await db.getAllCars()
     const categories = await db.getAllCategories()
-    res.render('index', {categories: categories, cars: cars})
+    res.render('index', {
+        categories: categories, 
+        cars: cars, 
+        title: 'Auto Warehouse'
+    })
 }
 
 
